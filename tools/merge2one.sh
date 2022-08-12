324748763a503390cf4b8cff3cc7a223e2f51462
GL <gonglei@gotechgames.com>
2022-08-12 15:52:49 +0800

    更新文档

--- tools/merge2one.sh
+++ tools/merge2one.sh
@@ -1,1 +1,1 @@
-montage ../exports/*.png -resize 400x *  -mode Concatenate -tile 6x -geometry +5+5 ../output/overview.png
+montage ../exports/*.png -resize 400x *  -mode Concatenate -tile 6x -geometry +5+10 ../output/overview.png
