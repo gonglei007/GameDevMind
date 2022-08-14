17b13cf8742cdb53068300acea5de62dea8c9945
GL <gonglei@gotechgames.com>
2022-08-14 16:25:14 +0800

    更新overview图

--- tools/merge2one.sh
+++ tools/merge2one.sh
@@ -1,1 +1,1 @@
-montage $(ls ../exports/*.png | grep -v "0.") -resize 400x *  -mode Concatenate -tile 7x -geometry +5+10 ../overview/overview.png
+montage $(ls ../exports/*.png | grep -v "0.") -resize 400x *  -mode Concatenate -tile 7x -geometry +20+40 ../overview/overview.png
