e47c2b77a7fd9cc36bddfb9439928b263d403719
GL <gonglei@hotmail.com>
2022-11-09 00:46:11 +0800

    更新总览

--- tools/merge-overview.sh
+++ tools/merge-overview.sh
@@ -3,4 +3,7 @@ montage ../exports/0.总览.png ../exports-en/0.Overview.png 	\
 	-auto-orient 	\
 	-geometry 4096x4096 \
 	-mode Concatenate	\
+	-border 1 	\
+	-bordercolor LightSalmon4    \
+	-background "#E9E298"   \
 	../output/overview.png
