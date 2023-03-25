c44fcb1b4b2eb894913694e9d5f05a4081908f0e
GL <gonglei@hotmail.com>
2023-03-26 00:20:56 +0800

    更新overview

--- tools/groups/mergeGroup4.sh
+++ tools/groups/mergeGroup4.sh
@@ -7,7 +7,7 @@ ItemTitle=5x
 echo "合成图4"
 montage \
     ../exports/4.1.游戏生产.png \
-    ../exports/4.1.1.数字内容生产线.png \
+    ../exports/4.1.1.数字内容生产.png \
     ../exports/4.1.2.AI助力游戏生产.png \
     ../exports/4.1.3.引擎的基本功能系统.png \
     ../exports/4.1.4.引擎的其它系统.png \
